bb2afb1826a69a1ad1cd7de8694551bc180f4240
Edwin Akhwale <edwin.akhwale@aln.africa>
2023-07-10 12:45:27 +0300

    implemented user auth using passport.js, altered the /add user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
-
+const session = require('express-session');
 let User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 // add user
 
-router.route('/add').post((req,res)=>{
-    const userName = req.body.userName;
-    const password = req.body.password;
+router.route('/add').post((req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
 
-    const newUser = new User(
-        {userName, password}
-    );
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      return res.status(500).json('Error hashing password.');
+    }
 
-    newUser.save()
-    .then(()=> res.json("User Added Successfully"))
-    .catch(err=>res.status(400).json("Error:" + err));
+    const newUser = new User({
+      userName,
+      password: hashedPassword,
+    });
+
+    newUser
+      .save()
+      .then(() => res.json('User Added Successfully'))
+      .catch((err) => res.status(400).json('Error:' + err));
+  });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,71 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+
 const app = express();
 const port = process.env.PORT;
 const uri = process.env.ATLAS_URI;
 app.use(cors(), express.json());
+
+
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
+const User = require('./models/user.model');
+
+
+passport.use(
+  new LocalStrategy((username, password, done)=>{
+    User.findOne({userName:username}, (err, user)=>{
+      if(err){
+        return done(err);
+      }
+      if (!user){
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      
+      bcrypt.compare(password, user.password, (err, result)=>{
+        if(result){
+          return done(null, user);
+        }
+        else{
+          return done(null, false, {message: "Wrog password"})
+        }
+
+      })
+
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 
 const usersRouter = require('./routes/user');
 
